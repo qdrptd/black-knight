890d8d0bdec8b344719242fb5ad7af497cb8a0a3
qdrptd <qdrptd@qdrptdui-MacBookPro.local>
2024-05-24 02:37:05 +0900

    board organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [board, setBoard] = useState([['M', 'B', 'B', 'B', 'B', 'R'],
+                                      ['N', 'N', 'N', 'N', 'R', 'R'],
+                                      ['W', 'W', 'W', 'W', 'R', '0']])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex items-center justify-center mt-16'>
+    <div>
+      {
+        board.map((value, x)=>{
+          return(
+            <div className='flex items-center'>
+              {
+            value.map((value, y) =>{
+
+              return(
+                <div>
+                {
+                  value === 'W'?
+                  <div className='w-40 h-40 bg-white'></div>
+                  :
+                  <div className={`w-40 h-40 ${(x+y)%2==0 ? 'bg-light' : 'bg-dark'}`}>
+                    {
+                      value === '0'?
+                      <div/>
+                      :
+                      <img src={`${value}.svg`} className='w-full h-full'></img>
+                    }
+                    
+                  </div>
+                }
+                </div>
+              )
+            })
+          }
+            <br/>
+            </div>
+          )
+        })
+      }
+    </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts,tsx}'],
+  theme: {
+    colors:{
+      'light': '#F0D9B5',
+      'dark': '#B58862',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
